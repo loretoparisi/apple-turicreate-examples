893072f1ba674bd1f4873724aac92487a251aa74
Loreto Parisi <loretoparisi@gmail.com>
2018-06-22 16:09:35 +0200

    recommender: predict by user_id

--- recommender/train.py
+++ recommender/train.py
@@ -14,7 +14,37 @@ import turicreate as tc
 
 DATASET_ROOT='/Users/loretoparisi/webservice/'
 
+# we use the MovieLens 20M dataset
 actions = tc.SFrame.read_csv(os.path.join(DATASET_ROOT,"ml-20m","ratings.csv"))
+
+print( actions[0:10] )
+
+# You may have additional data about users or items. For example we might have a dataset of movie metadata.
 items = tc.SFrame.read_csv(os.path.join(DATASET_ROOT,"ml-20m","movies.csv"))
+
+print( items[0:10] )
+
+# First we create a random split of the data to produce a validation set that can be used to evaluate the model.
+training_data, validation_data = tc.recommender.util.random_split_by_user(actions, 'userId', 'movieId')
+model = tc.recommender.create(training_data, 'userId', 'movieId')
+
+# The input parameter k controls how many items to recommend for each user.
+k=5
+
+# Now that you have a model, you can make recommendations
+top_recommendations = model.recommend(k=k)
+
+# print first (k*3) rows ( k results x 3 users) of 4 columns
+num_users=3
+top_recommendations.print_rows(num_rows=(k*num_users), num_columns=4)
+
+# The user names must correspond to strings in the user_id column in the training data.
+recommendations = {}
+recommendations['1'] = model.recommend(users=['1'])
+recommendations['2'] = model.recommend(users=['2'])
+
+# print recommendations by user
+for r in recommendations:
+    recommendations[r].print_rows(num_rows=k, num_columns=4)
 
 
